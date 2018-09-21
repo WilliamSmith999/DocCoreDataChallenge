736fb11f774805ec881e64a6d032caac2bb557fb
WilliamSmith999 <williamsmithtannis013@gmail.com>
2018-09-21 17:35:00 -0500

    Document initializer build

--- CoreDataDoc/Document+CoreDataClass.swift
+++ CoreDataDoc/Document+CoreDataClass.swift
@@ -7,10 +7,29 @@
 //
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 @objc(Document)
 public class Document: NSManagedObject {
-
+    var date: Date?{
+        get{
+            return rawDate as Date?
+        }
+        set{
+            rawDate = newValue as NSDate?
+        }
+    }
+    
+    convenience init?(name: String?, date: Date?, content: String) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {
+            return nil
+        }
+        self.init(entity: Document.entity(), insertInto: managedContext)
+            self.content = content
+            self.date = date
+            self.name = name
+        
+    }
 }
